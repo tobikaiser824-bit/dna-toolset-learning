ff6499619ac431d6d26ac3697809704460188992
Juris Laivins <redacted@users.noreply.github.com>
2020-03-05 07:54:02 +0000

    Video 4: Translation, Coidon Usage.

--- dna_toolkit.py
+++ dna_toolkit.py
@@ -1,6 +1,6 @@
 # DNA Toolkit file
 from structures import *
-# from collections import Counter
+from collections import Counter
 
 
 def validate_seq(seq):
@@ -54,3 +54,22 @@ def gc_content_subsec(seq, k=20):
         subseq = seq[i:i + k]
         res.append(gc_content(subseq))
     return res
+
+
+def translate_seq(seq, init_pos=0):
+    """Translates a DNA sequence into an aminoacid sequence"""
+    return [DNA_Codons[seq[pos:pos + 3]] for pos in range(init_pos, len(seq) - 2, 3)]
+
+
+def codon_usage(seq, aminoacid):
+    """Provides the frequency of each codon encoding a given aminoacid in a DNA sequence"""
+    tmpList = []
+    for i in range(0, len(seq) - 2, 3):
+        if DNA_Codons[seq[i:i + 3]] == aminoacid:
+            tmpList.append(seq[i:i + 3])
+
+    freqDict = dict(Counter(tmpList))
+    totalWight = sum(freqDict.values())
+    for seq in freqDict:
+        freqDict[seq] = round(freqDict[seq] / totalWight, 2)
+    return freqDict
