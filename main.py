9099a434d85d36665ef3bdaf0413a0a9fe5b7dac
RebelCoderRU <redacted@users.noreply.github.com>
2020-04-25 17:41:27 +0300

    Video 8: Class refactoring. Part 1

--- main.py
+++ main.py
@@ -1,38 +1,8 @@
 # DNA Toolset/Code testing file
-from dna_toolkit import *
-import random
 
-# Creating a random DNA sequence for testing:
-randDNAStr = ''.join([random.choice(DNA_Nucleotides)
-                      for nuc in range(50)])
+from bio_seq import bio_seq
 
-DNAStr = validate_seq(randDNAStr)
+test_dna = bio_seq()
+test_dna.generate_rnd_seq(40, "DNA")
 
-print(f'\nSequence: {DNAStr}\n')
-print(f'[1] + Sequence Length: {len(DNAStr)}\n')
-print(f'[2] + Nucleotide Frequency: {nucleotide_frequency(DNAStr)}\n')
-
-print(f'[3] + DNA/RNA Transcription: {transcription(DNAStr)}\n')
-
-print(f"[4] + DNA String + Complement + Reverse Complement:\n5' {DNAStr} 3'")
-print(f"   {''.join(['|' for c in range(len(DNAStr))])}")
-print(f"3' {reverse_complement(DNAStr)[::-1]} 5' [Complement]")
-print(f"5' {reverse_complement(DNAStr)} 3' [Rev. Complement]\n")
-
-print(f'[5] + GC Content: {gc_content(DNAStr)}%\n')
-print(
-    f'[6] + GC Content in Subsection k=5: {gc_content_subsec(DNAStr, k=5)}\n')
-
-print(
-    f'[7] + Aminoacids Sequence from DNA: {translate_seq(DNAStr, 0)}\n')
-
-print(
-    f'[8] + Codon frequency (L): {codon_usage(DNAStr, "L")}\n')
-
-print('[9] + Reading_frames:')
-for frame in gen_reading_frames(DNAStr):
-    print(frame)
-
-print('\n[10] + All prots in 6 open reading frames:')
-for prot in all_proteins_from_orfs(DNAStr, 0, 0, True):
-    print(f'{prot}')
+print(test_dna.get_seq_info())
