9e49a35c705c3309ab36d1106474c227af9a74f1
RebelCoderRU <redacted@users.noreply.github.com>
2020-05-04 11:48:47 +0300

    Video 8.2. bio_seq class completed.

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ test_dna = bio_seq()
 test_dna.generate_rnd_seq(40, "DNA")
 
 print(test_dna.get_seq_info())
+print(test_dna.nucleotide_frequency())
+print(test_dna.transcription())
+print(test_dna.reverse_complement())
+print(test_dna.gc_content())
+print(test_dna.gc_content_subsec())
+print(test_dna.translate_seq())
+print(test_dna.codon_usage('L'))
+
+for rf in test_dna.gen_reading_frames():
+    print(rf)
+
+
+print(test_dna.all_proteins_from_orfs())
